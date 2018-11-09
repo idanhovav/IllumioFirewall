3217a13dc2cea46ca664aff77fbe0626140cc6cd
Idan <idan.hovav16@gmail.com>
2018-11-09 15:19:04 -0800

    Add more basic tests
    All tests pass

--- testFirewall.py
+++ testFirewall.py
@@ -21,5 +21,10 @@ class TestFirewall(TestCase):
         self.assertTrue(not firewall.accept_packet("inbound", "tcp", 81, "192.168.1.2"))
         self.assertTrue(not firewall.accept_packet("inbound", "tcp", 80, "192.168.1.3"))
 
+        self.assertTrue(firewall.accept_packet("outbound", "udp", 13, "192.192.192.192"))
+        self.assertTrue(firewall.accept_packet("outbound", "udp", 12, "192.192.192.192"))
+        self.assertTrue(not firewall.accept_packet("outbound", "udp", 13, "192.192.192.194"))
+        self.assertTrue(not firewall.accept_packet("outbound", "tcp", 13, "192.192.192.192"))
+
 if __name__ == '__main__':
     unittest.main()
